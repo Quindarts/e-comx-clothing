68e478ba2365c4719a78de434182787bcd5c8f08
bird <quang82thcspb@gmail.com>
2023-07-08 16:32:15 +0700

    [ECC-5] colors, icons values

---
+++ client/src/utils/constants.ts
@@ -0,0 +1,63 @@
+export const ROUTE = {
+    //MAIN
+    INDEX: "/",
+    ACCOUNT: "/account",
+    COLLECTION: "/collection",
+    CART: "/cart",
+    SEARCH: "/search",
+    CHECKOUT: "/cart",
+    NOT_FOUND: "/not-found",
+    PRODUCT_DETAIL: "/product-detail",
+
+    //Authentication
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    FORGOT_PASSWORD: "/auth/forgot-pass",
+
+    //OTHERS
+    ABOUT: "/about",
+    CONTACT: "/contact",
+    BLOG: "/blog",
+};
+
+export const PRODUCT_CATEGORY = ["All Items", "Women", "Man", "Kids", "Jewels"];
+export const PRODUCT_SIZE = ["XS", "M", "L", "XL", "2XL"];
+export const PRODUCT_FILTER = [
+    "Most Popular",
+    "Best Rating",
+    "Price Low - Hight",
+    "Price Hight - Low",
+];
+
+export const ICON_LIBARY = {
+    i_search: "ep:search",
+    i_user: "ri:user-line",
+    i_cart: "fluent:cart-24-regular",
+    i_arrow_left: "cil:arrow-left",
+    i_arrow_right: "bi:arrow-right",
+    i_heart: "uil:heart",
+    i_dollar: "healthicons:dollar",
+    i_clock: "mdi:clock-outline",
+    i_setting: "uiw:setting-o",
+    i_star: "ic:round-star",
+    i_more_star: "lucide:stars",
+    i_man_circle: "ant-design:man-outlined",
+    i_female_circle: "mingcute:female-line",
+    i_filter: "cil:filter",
+    i_down: "ps:down",
+    i_close: "carbon:close-filled",
+    i_size: "radix-icons:size",
+    i_bag: "ph:bag",
+    i_screen: "ic:outline-fit-screen",
+    i_plus: "ic:outline-plus",
+    i_subtraction: "subway:subtraction-1",
+    i_exit: "system-uicons:exit-left",
+    i_help: "solar:help-line-duotone",
+    i_image_plus: "majesticons:image-plus-line",
+    i_phone: "ph:phone",
+    i_calendar: "bx:calendar",
+    i_address: "entypo:address",
+    i_shipping: "la:shipping-fast",
+    i_payment: "fluent:payment-16-regular",
+    i_tick: "mdi:tick",
+};
