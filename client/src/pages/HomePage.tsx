88ce78a0731733f1e80a8d46cef6e4476235f787
bird <quang82thcspb@gmail.com>
2023-07-08 14:55:24 +0700

    [ECC-6] Folder Structure

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+    return (
+        <div className="home_page">
+            HomePage
+            <div className="home_page-title">abec</div>
+        </div>
+    );
+}
+
+export default HomePage;
